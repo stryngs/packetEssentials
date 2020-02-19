65857ccbf65bbe2bffa238a5c3d128fe462f03f4
stryngs <support@configitnow.com>
2020-02-19 00:57:22 -0500

    updated

--- SRC/setup.py
+++ SRC/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name = 'packetEssentials',
-    version = '1.6.2',
+    version = '1.6.4',
     author = 'stryngs',
     author_email = 'support@configitnow.com',
     packages = ['packetEssentials', 'packetEssentials.lib'],
